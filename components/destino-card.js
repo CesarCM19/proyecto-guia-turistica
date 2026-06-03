ff8bbbbc663ccc6b34734b6f72862495c979bd6a
CesarCM19 <cesarcamposmora04@gmail.com>
2026-06-02 18:46:35 -0600

    Destino card clean

--- components/destino-card.js
+++ components/destino-card.js
@@ -1,8 +1,4 @@
 class DestinoCard extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-    }
 
     static get observedAttributes() {
         return [
